62d8f94dd9e8dd54867324f26ca5c2a6830c0273
Atharva Nanavate <85158270+AtharvaNanavate@users.noreply.github.com>
2022-05-26 01:19:54 +0530

    Create SUBSCRIBE.c

---
+++ START38D/SUBSCRIBE.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+int time, tc;
+
+scanf("%d",&tc);
+for(int i=0; i<tc; i++){
+
+scanf("%d",&time);
+if(time <= 30)
+  printf("No\n");
+else printf("Yes\n");}
+  return 0;
+}
